8118d8a86428a60c838b045e7a40f6361a9069a0
rimu-7 <rrimu99@gmail.com>
2025-09-03 01:28:38 +0600

    standardize import quotes and formatting in index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { connectDB } from './config/db.js';
-import { authRoutes } from './routes/authRoutes.js';
-import { urlRoutes } from './routes/urlRoutes.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import { authRoutes } from "./routes/authRoutes.js";
+import { urlRoutes } from "./routes/urlRoutes.js";
 import useragent from "express-useragent";
-import { userRoutes } from './routes/userRoutes.js';
+import { userRoutes } from "./routes/userRoutes.js";
 
 const app = express();
 app.use(cors());
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(express.json());
 app.use(useragent.express());
 connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello There!");
+});
 authRoutes(app);
 urlRoutes(app);
 userRoutes(app);
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port 3000`);
 });

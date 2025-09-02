e9adaa83c8d9dc72fbc1a6859b29468b975b0c00
rimu-7 <rrimu99@gmail.com>
2025-09-03 01:44:53 +0600

    simplify CORS configuration by using default settings

--- server/index.js
+++ server/index.js
@@ -12,26 +12,8 @@ const app = express();
 // ========================
 // CORS Configuration
 // ========================
-const allowedOrigins = [
-  "https://easyurl-online.netlify.app/", 
-  "http://localhost:5173",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman, mobile apps)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
+app.options("*", cors()); 
 
 // ========================
 // Middleware
